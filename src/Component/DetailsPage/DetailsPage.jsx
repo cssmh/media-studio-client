1a89ecd2f733007830ced48ba32baad83e974d1c
momen210 <touristmomen@gmail.com>
2024-02-12 19:26:01 +0600

    cart cine add to database added

--- src/Component/DetailsPage/DetailsPage.jsx
+++ src/Component/DetailsPage/DetailsPage.jsx
@@ -1,20 +1,35 @@
 import { useLoaderData } from "react-router-dom";
+import swal from "sweetalert";
 
 const DetailsPage = () => {
   const loadDetailsData = useLoaderData();
   //   console.log(loadDetailsData);
-  const { name, image, price, description } = loadDetailsData;
+  const { name, image, media, media_type, price, description, rating } =
+    loadDetailsData;
+  // if i sent loadDetailsData to database, it will send its id also that will be same $ i won't be able add duplicate. Thats why i send dataToDatabase which don't have id. database gonna gave it unique id.
+  const dataToDatabase = {
+    name: name,
+    image: image,
+    media: media,
+    media_type: media_type,
+    price: price,
+    description: description,
+    rating: rating,
+  };
   const handleAddCart = () => {
     fetch("http://localhost:5000/cart", {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(loadDetailsData),
+      body: JSON.stringify(dataToDatabase),
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        if (data.insertedId) {
+          swal("Good job!", `${name} added to cart`, "success");
+        }
       });
   };
 
