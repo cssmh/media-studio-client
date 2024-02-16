9dc5771111cee4eb3454567cd2e1fcc18b3e436c
momen210 <touristmomen@gmail.com>
2024-02-16 21:23:48 +0600

    dynamic media  type name added, search option added

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,5 +1,5 @@
-import banner1 from "./../../../src/assets/2.jpg";
-import banner2 from "./../../../src/assets/1.png";
+import banner2 from "./../../../src/assets/2.jpg";
+import banner1 from "./../../../src/assets/1.png";
 import banner3 from "./../../../src/assets/3.jpg";
 
 const Banner = () => {
--- src/Component/CineDetails/CineDetails.jsx
+++ src/Component/CineDetails/CineDetails.jsx
@@ -5,6 +5,9 @@ import CineDetailCard from "../CineDetailCard/CineDetailCard";
 
 const CineDetails = () => {
   const [matching, setMatching] = useState([]);
+  const [filter, setFilter] = useState([]);
+  // console.log(matching);
+  let searchTerm;
   // console.log(matching);
 
   const loaderData = useLoaderData();
@@ -14,19 +17,45 @@ const CineDetails = () => {
     const matchBoth = loaderData.filter((data) => data.media === paramsName.id);
     // console.log(matchBoth);
     setMatching(matchBoth);
+    setFilter(matchBoth);
   }, [loaderData, paramsName]);
 
   return (
     <div>
       <Banner></Banner>
-      <h1 className="text-2xl text-center font-semibold mt-5">CineMIX</h1>
-      {matching.length == 0 ? (
+      <h1 className="text-2xl text-center font-semibold mt-5">
+        {`${paramsName.id} Categories`}
+      </h1>
+      {/* search */}
+      <div className="text-center my-6">
+        <input
+          type="text"
+          name="name"
+          placeholder="Search Here"
+          className="input input-bordered w-80 rounded-xl border-purple-500"
+          onChange={(e) => {
+            searchTerm = e.target.value;
+            // console.log(searchTerm);
+
+            if (searchTerm === "") {
+              setFilter(matching);
+            } else {
+              const searchItem = matching.filter((data) =>
+                data.name.toLowerCase().includes(searchTerm.toLowerCase())
+              );
+              setFilter(searchItem);
+            }
+          }}
+        />
+      </div>
+      {/* search end */}
+      {filter.length == 0 ? (
         <p className="text-center my-12 text-red-500">No data available</p>
       ) : (
         ""
       )}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 max-w-5xl mx-auto my-12">
-        {matching.map((data) => (
+        {filter.map((data) => (
           <CineDetailCard key={data._id} getCard={data}></CineDetailCard>
         ))}
       </div>
