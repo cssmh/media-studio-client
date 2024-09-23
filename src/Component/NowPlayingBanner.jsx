c328ea28e0811fa2497974b3f106b3dcd33fddc7
cssmh <touristmomen@gmail.com>
2024-09-23 10:32:01 +0600

    playing banner added

---
+++ src/Component/NowPlayingBanner.jsx
@@ -0,0 +1,10 @@
+import banner from "../assets/img_66.png"
+const NowPlayingBanner = () => {
+    return (
+        <div>
+            <img src={banner} alt="" />
+        </div>
+    );
+};
+
+export default NowPlayingBanner;
